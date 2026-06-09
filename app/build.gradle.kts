acadb6c1040ec6cbd18666a6ba27205e1431fda7
Ricardo Soto <ricardosr4@gmail.com>
2026-06-09 14:29:50 -0400

    build: apply initial application configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.kotlinlabauthcore"
-        minSdk = 35
+        minSdk = 26
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
