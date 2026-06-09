e68e2f2208a397310dc9c40ea7840075a6a703b1
Ricardo Soto <ricardosr4@gmail.com>
2026-06-09 14:25:18 -0400

    docs: add initial project documentation
    build: update initial Android dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.kotlinlabauthcore"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.kotlinlabauthcore"
         minSdk = 35
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
